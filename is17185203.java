dcd6a96ca376162fe63f917613383851e5f8c4ad
Tomk1111 <tomk.tk53@gmail.com>
2019-04-10 20:08:58 +0100

    Add files via upload
    Removed old code

--- is17185203.java
+++ is17185203.java
@@ -2,24 +2,24 @@ import java.io.*;
 import javax.swing.*;
 import java.util.*;
 import java.util.Collections;
+
+
+/* 
+ * Thomas Kiely - 17185203
+ * Sean Morrissey - 17222761
+ * Paul Murphy - 17198046
+ * Mark McNabola - 17226163 
+*/
+
+
 public class is17185203
 {
 	private static boolean full=false;
-	private static ArrayList<int[][]> open = new ArrayList<int[][]>();
-	private static ArrayList<Integer> openCosts = new ArrayList<Integer>(); //HVALUE
-	private static ArrayList<Integer> openLevel = new ArrayList<Integer>(); //GVALUE
-	private static ArrayList<int[][]> closed = new ArrayList<int[][]>();
 	
 	private static ArrayList<Node> openNodes = new ArrayList<Node>();
 	private static ArrayList<Node> closedNodes = new ArrayList<Node>();
-	
-	
-	
-	
-	
-	
-	
-	private static ArrayList<Node> bestMoves = new ArrayList<Node>();
+	private static ArrayList<Node> bestPath = new ArrayList<Node>();
+
 	private static int gValue = 0;
 	private static int size=0;
 	private final static String message1="Please enter the Start state as a sequence of numbers between [0, 8] e.g 5 2 0 6 1 3 4 8 7";
@@ -28,7 +28,7 @@ public class is17185203
 public static void main(String[] args) throws Exception
 {
 	setSize();
-	int finall[][];
+	int origin[][];
 	int goal[][];
 	String message;
 	if(size==3)
@@ -36,18 +36,15 @@ public static void main(String[] args) throws Exception
 	else 
 		message=message2;
 	boolean valid=false;
-	finall=new int[3][3];
-	finall=fill(message);
+	origin=new int[3][3];
+	origin=fill(message);
 	goal=new int[3][3];
 	goal=fill(message.replaceAll("Start","End"));
-	int posx=getx(finall);
-	int posy=gety(finall);
-
-	int cost = getCost(finall, goal);
-	Node sourceNode = new Node(finall, gValue,cost);
+	int posx=getx(origin);
+	int posy=gety(origin);
+	int cost = getCost(origin, goal);
+	Node sourceNode = new Node(origin, gValue,cost);
 	openNodes.add(sourceNode);
-//	printBoard(finall);
-	//System.out.println();
 	solveit(sourceNode, posx, posy, goal);
 }
 
@@ -57,21 +54,16 @@ public static void solveit(Node source,int x,int y,int [][]goal)
 {
 	try
 	{	
-		ArrayList<Node> bestPath = new ArrayList<Node>();
 		boolean check = false;
 		if (compareBoards(source.board, goal))	
 		{
-			System.out.println("WIN");
 			while (!check)
 			{
 				bestPath.add(source);
 				
 
 				if (source.parent == null)
-				{
-					check = true;
-					
-				}
+					check = true;		
 				else
 					source = source.parent;
 			}
@@ -79,13 +71,9 @@ public static void solveit(Node source,int x,int y,int [][]goal)
 			for (Node tempN : bestPath)
 			{
 				printBoard(tempN.board);
-				System.out.println("---------");
 				
 			}
 		}
-			
-			//System.out.println("You win");
-			//printBoard(source);
 		
 		else
 		{
@@ -96,7 +84,6 @@ public static void solveit(Node source,int x,int y,int [][]goal)
 			int tempx=0;
 			if(y+1<source.board.length)
 				{
-				//	System.out.println("(B): "+source[x][y+1] + " to the west");
 					temp=copy(source.board);
 					tempx =temp[x][y+1];
 					temp[x][y+1]=0;
@@ -104,19 +91,13 @@ public static void solveit(Node source,int x,int y,int [][]goal)
 	
 					if (!boardSearched(temp))
 					{
-					//	printBoard(temp);
-					//	System.out.println();
-					//	System.out.println("Cost =" +(getCost(temp,goal) + gValue));
 						Node tempNode = new Node(temp, gValue, getCost(temp, goal), source);
 						openNodes.add(tempNode);
-				//		open.add(temp);
-					//	openCosts.add(getCost(temp, goal));
-						//openLevel.add(gValue);
+
 					}
 				}
 				if(y-1>=0)
 				{
-				//	System.out.println("(A): "+source[x][y-1] +" to the east");
 					temp=copy(source.board);
 					tempx =temp[x][y-1];
 					temp[x][y-1]=0;
@@ -127,18 +108,11 @@ public static void solveit(Node source,int x,int y,int [][]goal)
 						
 						Node tempNode = new Node(temp, gValue, getCost(temp, goal), source);
 						openNodes.add(tempNode);
-					//	printBoard(temp);
-					//	System.out.println();
-					//	System.out.println("Cost =" +(getCost(temp,goal) + gValue));
-						//open.add(temp);
-						//openCosts.add(getCost(temp, goal));
-						//openLevel.add(gValue);
 					}
 				}
 				
 				if(x-1>=0)
 				{
-				//	System.out.println("(C): "+source[x-1][y] + " to the south");
 					temp=copy(source.board);
 					tempx =temp[x-1][y];
 					temp[x-1][y]=0;
@@ -148,18 +122,11 @@ public static void solveit(Node source,int x,int y,int [][]goal)
 					{
 						Node tempNode = new Node(temp, gValue, getCost(temp, goal), source);
 						openNodes.add(tempNode);
-					//	printBoard(temp);
-					//	System.out.println();
-					//	System.out.println("Cost =" +(getCost(temp,goal) + gValue));	
-					//	open.add(temp);
-					//	openCosts.add(getCost(temp, goal));
-					//	openLevel.add(gValue);
 					}
 				}
 				
 				if(x+1<source.board.length)
 				{
-				//	System.out.println("(D): "+source[x+1][y] + " to the north");
 					temp=copy(source.board);
 					tempx =temp[x+1][y];
 					temp[x+1][y]=0;
@@ -169,12 +136,6 @@ public static void solveit(Node source,int x,int y,int [][]goal)
 					{
 						Node tempNode = new Node(temp, gValue, getCost(temp, goal), source);
 						openNodes.add(tempNode);
-					//	printBoard(temp);
-					//	System.out.println();
-					//	System.out.println("Cost =" +(getCost(temp,goal) + gValue));
-						//open.add(temp);
-						//openCosts.add(getCost(temp, goal));
-						//openLevel.add(gValue);
 					}
 				}
 				
@@ -183,53 +144,8 @@ public static void solveit(Node source,int x,int y,int [][]goal)
 				closedNodes.add(openNodes.get(0));
 				openNodes.remove(0);
 				}
-				
-				
-				sortOpenList();
-			
-				//	System.out.println("Best Choice is:\n");
-				//printBoard(open.get(0));
-				//	System.out.println();
-
-				//Node move = new Node(open.get(0), gValue, getCost(open.get(0), goal), parentNode);
-				//bestMoves.add(parentNode);
-				//int pos = 0;
-			/*	if (bestMoves.size() > 0)
-				{
-						System.out.println("ln178 SIZE : " + bestMoves.size());
-							int smallest = bestMoves.get(bestMoves.size()-1).cost;
-							for (int i=bestMoves.size()-1;i>=0;i--)
-							{
-								System.out.println("ln183 SIZE : " + bestMoves.size());
-								if (bestMoves.get(i).cost < smallest)
-								{
-									System.out.println("ln186 SIZE : " + bestMoves.size());
-									smallest = bestMoves.get(i).cost;
-									pos = i;
-									parentNode = bestMoves.get(pos).parent;
-								}
-							}
-							
-							if (bestMoves.get(pos).gVal <=bestMoves.get(bestMoves.size()-1).gVal)
-							{
-								System.out.println("ln195 SIZE : " + bestMoves.size());
-								bestMoves.clear();
-								System.out.println("ln197 SIZE : " + bestMoves.size());
-								while(parentNode.parent != null)
-								{
-									System.out.println("ln199 SIZE : " + bestMoves.size());
-									bestMoves.add(parentNode);
-									if (parentNode.parent != null)
-									{
-										System.out.println("ln203 SIZE : " + bestMoves.size());
-										parentNode = parentNode.parent;
-									}
-								};
-							}
-				}*/
-				//int[][] newSource = open.get(0);
-				
-			
+						
+				sortOpenList();	
 				
 				Node newSource = openNodes.get(0);
 				int posX = getx(newSource.board);
@@ -247,15 +163,18 @@ public static void solveit(Node source,int x,int y,int [][]goal)
 		{
 			for (int j=0;j < board.length;j++)
 			{
-				System.out.print(board[i][j] + " ");
+					System.out.print(board[i][j] + " ");
+				
+				
 			}
 			System.out.println();
 		}
+		System.out.println("--------");
 	}
 	
 	
 	
-	public static void sortOpenList() //BUBBLE SORT FOR NOW
+	public static void sortOpenList() 
 	{		
 		Node tempNode;
 		
@@ -290,19 +209,19 @@ public static void solveit(Node source,int x,int y,int [][]goal)
 	
 	public static boolean boardSearched(int[][] board)
 	{
-		if (open.size() > 0)
+		if (openNodes.size() > 0)
 		{
-			for (int i=0;i < open.size();i++)
+			for (int i=0;i < openNodes.size();i++)
 			{
-				if (compareBoards(board, open.get(i)))
+				if (compareBoards(board, openNodes.get(i).board))
 					return true;
 			}
 		}
-		if (closed.size() > 0)
+		if (closedNodes.size() > 0)
 		{
-			for (int i=0;i < closed.size();i++)
+			for (int i=0;i < closedNodes.size();i++)
 			{
-				if (compareBoards(board, closed.get(i)))
+				if (compareBoards(board, closedNodes.get(i).board))
 					return true;
 			}
 		}
@@ -337,7 +256,6 @@ public static void solveit(Node source,int x,int y,int [][]goal)
 				if(a[i][z]==0)
 				{
 				answ=i;
-				
 				}
 			}
 		}
@@ -354,9 +272,7 @@ public static void solveit(Node source,int x,int y,int [][]goal)
 				if(a[i][z]==0)
 				{
 				ans=z;
-				
-				}
-				
+				}	
 			}
 		}
 		return ans;
@@ -454,8 +370,7 @@ public static int [][] fill(String text)
 				if(values[i][j]!=0)
 				{
 					if(values[i][j]!=goal[i][j])
-						val++;
-					
+						val++;	
 				}
 			}
 		}
@@ -470,10 +385,7 @@ public static int [][] fill(String text)
 		for (int i=0;i<source.length;i++)
 		{
 			for(int j=0;j<source.length;j++)
-			{
-			temp[i][j]=source[i][j];
-			}
-		
+				temp[i][j]=source[i][j];
 		}
 		return temp;
 	}
@@ -487,7 +399,6 @@ public static int [][] fill(String text)
 		else
 			size=4;
 	}
-	
 	
 static class Node{  
 	 public  int[][] board;
@@ -509,13 +420,10 @@ static class Node{
 	{
 		this.board= board;
 		this.gVal = gVal;
-		this.hVal = hVal;
+			this.hVal = hVal;
 		this.cost = gVal + hVal;
 	}
-	
-	
-	
   }  
 }  
 	
-	
+		
